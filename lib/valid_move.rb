3e13c1108905383f08324b72b7ab4ba5134fa021
Nikoloz Chapichadze <nikoloz.chapichadze@gmail.com>
2020-09-24 02:09:01 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,7 @@
 # code your #valid_move? method here
-def tester
-    if (position_taken?)
-        
+def valid_move?(board, index)
+    if (position_taken?(board,index) || index)
+        return false
     else
         
     end
